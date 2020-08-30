00266d122b7a9d9bc339e01ee0d59e3647a37aee
CodingWithRigved <70466297+CodingWithRigved@users.noreply.github.com>
2020-08-30 13:36:53 +0530

    Files from 29 - 08 - 2020

---
+++ cap.py
@@ -0,0 +1,6 @@
+def cap_text(text):
+	'''
+	Input a String
+	Output a capitalized String
+	'''
+	return text.title()	
---
+++ pylint1.py
@@ -0,0 +1,13 @@
+'''
+A VERY SIMPLE SCRIPT
+'''
+def myfunc():
+    '''
+    a simple func
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
